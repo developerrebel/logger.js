a236c2974a4d153f9e49a7537301e7501e65d77d
developerrebel <cssherwood03@gmail.com>
2019-01-06 16:43:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const util = require('util');
+module.exports = (overwrite) => {
+    if(!overwrite) {
+
+    }
+	console.log = message => {
+		process.stdout.write(
+			`[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(Date.now())}]: ${
+				typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
+			}`
+		);
+	};
+}
