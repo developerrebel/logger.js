309e382b090b2bc043d34cd5ddf73d145d8a58e4
developerrebel <cssherwood03@gmail.com>
2019-01-07 13:55:31 -0700

    add full support for file logging

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const util = require('util');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function(overwrite, logPath) {
+module.exports = async function(overwrite, logPath) {
 	const currentDir = process.cwd();
 	if (logPath) {
-		if (!fs.existsSync(path.join(currentDir, logPath))) {
-			fs.mkdir(path.join(currentDir, logPath), err => {
-				if (err) console.error(err);
-			});
+		async function createPath() {
+			if (!fs.existsSync(path.join(currentDir, logPath))) {
+				const splitPath = logPath.split('/');
+				if (splitPath.length > 1) {
+					if (!fs.existsSync(path.join(currentDir, splitPath[0]))) splitPath.splice(0, 1);
+					let finishedDirectory = [];
+					await splitPath.forEach(file => {
+						fs.mkdir(path.join(currentDir, finishedDirectory.join('/'), file), () => {});
+						finishedDirectory.push(file);
+					});
+				} else {
+					fs.mkdir(path.join(currentDir, logPath));
+				}
+			}
+			if (!fs.existsSync(path.join(currentDir, logPath, '/stdout.log'))) {
+				fs.writeFile(path.join(currentDir, logPath, 'stdout.log'), '', err => {
+					if (err) console.error(err);
+				});
+				fs.writeFile(path.join(currentDir, logPath, 'stderr.log'), '', err => {
+					if (err) console.error(err);
+				});
+			}
 		}
 
-		if (!fs.existsSync(path.join(currentDir, logPath, '/stdout.log'))) {
-			fs.writeFile(path.join(currentDir, logPath, 'stdout.log'), '', err => {
-				if (err) console.error(err);
-			});
-			fs.writeFile(path.join(currentDir, logPath, 'stderr.log'), '', err => {
-				if (err) console.error(err);
-			});
-		}
-
+		await createPath();
 		log = message =>
 			fs.appendFile(path.join(currentDir, logPath, 'stdout.log'), message, err =>
 				err ? console.error(err) : {}
