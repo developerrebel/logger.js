73e63e58550395293f7fcfbc0aa334e6e2df1681
developerrebel <cssherwood03@gmail.com>
2019-01-06 23:19:40 -0700

    partially added log file support

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 const util = require('util');
 const fs = require('fs');
+const path = require('path');
 
-module.exports = function({ overwrite }) {
+module.exports = function(overwrite, logPath) {
+	const currentDir = process.cwd();
+	if (logPath) {
+		if (!fs.existsSync(path.join(currentDir, logPath))) {
+			fs.mkdir(path.join(currentDir, logPath), err => {
+				if (err) console.error(err);
+			});
+		}
+
+		if (!fs.existsSync(path.join(currentDir, logPath, '/stdout.log'))) {
+			fs.writeFile(path.join(currentDir, logPath, 'stdout.log'), '', err => {
+				if (err) console.error(err);
+			});
+			fs.writeFile(path.join(currentDir, logPath, 'stderr.log'), '', err => {
+				if (err) console.error(err);
+			});
+		}
+
+		log = message =>
+			fs.appendFile(path.join(currentDir, logPath, 'stdout.log'), message, err =>
+				err ? console.error(err) : {}
+			);
+		log(
+			`----------------------- New Process [${new Date().toLocaleDateString(
+				Date.now()
+			)} ${new Date().toLocaleTimeString(Date.now())}] -----------------------\n`
+		);
+	}
+
 	if (overwrite) {
 		console.log = message => {
-			process.stdout.write(
-				`[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(Date.now())}]: ${
-					typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
-				}`
-			);
+			const logMessage = `[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(
+				Date.now()
+			)}]: ${
+				typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
+			}`;
+
+			const logMessageStandard = `[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(
+				Date.now()
+			)}]: ${typeof message === 'object' ? '\n' + util.inspect(message) + '\n\n' : message + '\n'}`;
+
+			process.stdout.write(logMessage);
+			log(logMessageStandard);
 		};
 	}
 
 	return message => {
-		process.stdout.write(
-			`[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(Date.now())}]: ${
-				typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
-			}`
-		);
+		const logMessage = `[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(
+			Date.now()
+		)}]: ${typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'}`;
+
+		const logMessageStandard = `[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(
+			Date.now()
+		)}]: ${typeof message === 'object' ? '\n' + util.inspect(message) + '\n\n' : message + '\n'}`;
+
+		process.stdout.write(logMessage);
+		log(logMessageStandard);
 	};
 };
