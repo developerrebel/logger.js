0935b1db092378a9380a03cbd55fbc94bafbf1f4
developerrebel <cssherwood03@gmail.com>
2019-01-06 17:53:44 -0700

    add support for noOverwrite

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const util = require('util');
-module.exports = noOverwrite => {
-	if (noOverwrite) {
+const fs = require('fs');
+
+module.exports = function({ overwrite }) {
+	if (overwrite) {
+		console.log = message => {
+			process.stdout.write(
+				`[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(Date.now())}]: ${
+					typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
+				}`
+			);
+		};
 	}
 
-	console.log = message => {
+	return message => {
 		process.stdout.write(
 			`[${new Date().toLocaleDateString(Date.now())} ${new Date().toLocaleTimeString(Date.now())}]: ${
 				typeof message === 'object' ? '\n' + util.inspect(message, false, 2, true) + '\n\n' : message + '\n'
